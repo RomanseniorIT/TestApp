e14102c8cd6c52b59b1977236a656eb27278664e
Roman Lazuko <lazuko.romann@gmail.com>
2022-09-22 12:33:54 +0300

    implement view binding

--- app/src/main/java/com/ramanandroiddev/testapp/MainActivity.kt
+++ app/src/main/java/com/ramanandroiddev/testapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.ramanandroiddev.testapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ramanandroiddev.testapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initListeners()
+        initObservers()
+    }
+
+    private fun initListeners() {
+        binding.btnNextCat.setOnClickListener {
+
+        }
+    }
+
+    private fun initObservers() {
+
     }
 }
