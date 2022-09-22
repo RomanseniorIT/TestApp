d2f6c64b8e547d3690337425d279fc3c81ea9689
Roman Lazuko <lazuko.romann@gmail.com>
2022-09-22 17:50:12 +0300

    add repository implementation

--- app/src/main/java/com/ramanandroiddev/testapp/di/modules/RepositoryModule.kt
+++ app/src/main/java/com/ramanandroiddev/testapp/di/modules/RepositoryModule.kt
@@ -1,18 +1,13 @@
 package com.ramanandroiddev.testapp.di.modules
 
+import com.ramanandroiddev.testapp.data.MainRepositoryImpl
 import com.ramanandroiddev.testapp.domain.MainRepository
-import com.ramanandroiddev.testapp.domain.models.CatImage
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 
 @Module
-class RepositoryModule {
+interface RepositoryModule {
 
-    //Todo("implement repository")
-    @Provides
-    fun provideRepository(): MainRepository = object : MainRepository {
-        override suspend fun getCatImage(): CatImage {
-            return CatImage("")
-        }
-    }
+    @Binds
+    fun provideRepository(mainRepositoryImpl: MainRepositoryImpl): MainRepository
 }
